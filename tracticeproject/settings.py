be7e85e011ab02f50ef6e87f9fa8119136705fbd
Zach Mosher <zmosher4@gmail.com>
2024-10-17 17:29:59 -0500

    Remove tracked sqlite3 files and add to .gitignore

--- tracticeproject/settings.py
+++ tracticeproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'tracticeapi',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "tracticeproject.urls"
@@ -133,3 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  # Ensure all views require authentication
     ],
 }
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Your Next.js frontend URL
+]

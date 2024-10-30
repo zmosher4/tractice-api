73fe6ccd8e4b0605db9692bae5e23e8a6785e87e
Zach Mosher <zmosher4@gmail.com>
2024-10-30 12:46:43 -0500

    updated settings for deployment

--- tracticeproject/wsgi.py
+++ tracticeproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tractice.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracticeproject.settings")
 
 application = get_wsgi_application()

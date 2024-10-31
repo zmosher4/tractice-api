ed56d526bd53f98b623d07cb0a4b11d98881cb03
Zach Mosher <zmosher4@gmail.com>
2024-10-31 11:50:51 -0500

    updated settings

--- tracticeproject/settings.py
+++ tracticeproject/settings.py
@@ -144,9 +144,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tracticeapi/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'tracticeapi/static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
